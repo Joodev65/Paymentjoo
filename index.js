12fee11519e928a6fa56caf0bceacdde7a478b68
JooCode | Programmer <joomodz5@gmail.com>
2025-06-26 01:58:31 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 const cors = require("cors");
 const path = require("path");
 
+const app = express();
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type']
 }));
 
-app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
