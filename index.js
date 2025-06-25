1821e1cd83539334a73c1c281dbe0bad49aec8b2
JooCode | Programmer <joomodz5@gmail.com>
2025-06-26 01:35:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
